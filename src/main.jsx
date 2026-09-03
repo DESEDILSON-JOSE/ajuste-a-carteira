de3c42ec8d56c9b29e6106259615d61a2d952005
Desedilson Jose <desedilson@hotmail.com>
2026-09-03 14:12:18 -0300

    fix: wrap App with AppProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AppProvider } from './context/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null,
+  React.createElement(AppProvider, null,
     React.createElement(App, null)
   )
 )
